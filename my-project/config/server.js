91488dc91bb2fca31e6265ff659a373324b66c64
marcolaco85 <91003620+marcolaco85@users.noreply.github.com>
2021-09-19 15:22:02 +0200

    Installazione di Strapi

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '37f01e7c9137df17152180fe23c9fb0b'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af0318ab-2f69-4b83-8a18-693c0a2743b8'
+};
